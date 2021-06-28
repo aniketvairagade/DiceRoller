f87a3cec08850599508039c3414354b9e056c48a
aniketvairagade <aniketvairagade2000@gmail.com>
2021-06-28 17:11:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn1 : Button = findViewById(R.id.btn1)
+
+        val text : TextView = findViewById(R.id.tvRoll)
+
+        val tvRoll : ImageView = findViewById(R.id.dicePhase)
+
+        btn1.setOnClickListener {
+            val randomNum = (1..6).random()
+
+            text.text = randomNum.toString()
+
+            if (randomNum==1)
+            {
+                tvRoll.setImageResource(R.drawable.dice1)
+            }
+            else if (randomNum==2)
+            {
+                tvRoll.setImageResource(R.drawable.dice2)
+            }
+            else if (randomNum==3)
+            {
+                tvRoll.setImageResource(R.drawable.dice3)
+            }
+            else if (randomNum==4)
+            {
+                tvRoll.setImageResource(R.drawable.dice4)
+            }
+            else if (randomNum==5)
+            {
+                tvRoll.setImageResource(R.drawable.dice5)
+            }
+            else if (randomNum==6)
+            {
+                tvRoll.setImageResource(R.drawable.dice6)
+            }
+        }
+    }
+}
